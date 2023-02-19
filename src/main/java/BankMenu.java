ea08b282efed6cecc99ef64298af7ce239eef352
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-19 16:55:41 +0000

    lab completed

--- src/main/java/BankMenu.java
+++ src/main/java/BankMenu.java
@@ -2,13 +2,18 @@
 import java.util.Scanner;
 
 /**
- * Let's build a slightly more complicated app than the guessing game. This app can represent interacting with someone's
- * bank account. You will need to implement the methods in BankService for a fully functioning application. The app will
+ * Let's build a slightly more complicated app than the guessing game. This 
+ * app can represent interacting with someone's
+ * bank account. You will need to implement the methods in BankService for a 
+ * fully functioning application. The app will
  * be considered functional when all test cases pass.
  *
- * There is no need to change anything in this class. It does not affect the outcome of the test cases.
- * The purpose of this class is for you to interact with a functional Console Line menu, so that you can
- * see how your code works from the user's end. You can run this method and interact with the app through your
+ * There is no need to change anything in this class. It does not affect the 
+ * outcome of the test cases.
+ * The purpose of this class is for you to interact with a functional Console
+ * Line menu, so that you can
+ * see how your code works from the user's end. You can run this method and 
+ * interact with the app through your
  * IDE's terminal.
  */
 public class BankMenu {
@@ -17,7 +22,8 @@ public class BankMenu {
         BankService bankService = new BankService();
         boolean banking = true;
         while(banking){
-            System.out.println("Welcome to the bank! Please select what you'd like to do:");
+            System.out.println("Welcome to the bank! Please select" + 
+                             " what you'd like to do:");
             System.out.println("1. deposit");
             System.out.println("2. withdraw");
             System.out.println("3. view balance");
